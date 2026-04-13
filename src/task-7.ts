db9bd6fbc5ff18316c38033a52eca290c7365eeb
Dmitriy0212 <demarr68@gmail.com>
2026-04-14 00:27:11 +0300

    fix14

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,7 +1,7 @@
 const getMessage = (): Promise<string> => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Hello from TS да");
+      resolve("Hello from TS");
     }, 1000);
   });
 };
